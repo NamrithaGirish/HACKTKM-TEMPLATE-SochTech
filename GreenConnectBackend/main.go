b75a2835b43aefd282b97bc0f5ba69a8089a224a
NamrithaGirish <namrithac1723@gmail.com>
2024-02-18 08:43:43 +0530

    socket change

--- GreenConnectBackend/main.go
+++ GreenConnectBackend/main.go
@@ -20,11 +20,41 @@ func main() {
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT"},
-		AllowHeaders:     []string{"Content-Type", "Authorization", "ngrok-skip-browser-warning"},
+		AllowHeaders:     []string{"Content-Type", "Authorization", "ngr "},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12, // Maximum age in seconds
 	}))
+	// server, err := socketio.NewServer(nil)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// server.OnConnect("/", func(s socketio.Conn) error {
+	// 	log.Println("New connection:", s.ID())
+	// 	return nil
+	// })
+
+	// server.OnEvent("/", "chat message", func(s socketio.Conn, msg string) {
+	// 	log.Printf("[%s] Message: %s\n", s.ID(), msg)
+	// 	// Broadcast the received message to all connected clients
+	// 	server.BroadcastToRoom("/", "chat", "message", msg)
+	// })
+
+	// server.OnError("/", func(e error) {
+	// 	log.Println("Socket.IO error:", e)
+	// })
+
+	// server.OnDisconnect("/", func(s socketio.Conn, reason string) {
+	// 	log.Printf("[%s] Disconnected: %s\n", s.ID(), reason)
+	// })
+
+	// http.Handle("/socket.io/", server)
+	// http.Handle("/", http.FileServer(http.Dir("./public")))
+
+	// log.Println("Server started at :3000")
+	// log.Fatal(http.ListenAndServe(":3000", nil))
+
 	router.GET("/prev-mentor/:id", controllers.PrevMentors)
 	router.GET("/other-mentor/:id", controllers.GetAllOtherMentors)
 	router.GET("/other-mentor/name/:id", controllers.GetMentorsByName)
